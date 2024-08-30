e5ea6673a0c23b089c19c906d255639beb4d09ce
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-30 16:26:46 +0100

    update: live url to server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://vote-server-ahg0.onrender.com";
-const baseURL = "http://localhost:5001";
+const baseURL = "https://vote-server-ahg0.onrender.com";
+// const baseURL = "http://localhost:5001";
 
 export default axios.create({
   baseURL,
