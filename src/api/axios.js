1f39fdcc65f29ee2577dcbea7409996208baeb43
Amin <forkahamin@yahoo.co.uk>
2024-09-14 14:58:16 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
-// const baseURL = "https://vote-server-ahg0.onrender.com";
-const API_URL = import.meta.env.VITE_API_URL;
+const baseURL = "https://vote-server-ahg0.onrender.com";
+// const API_URL = import.meta.env.VITE_API_URL;
 
 export default axios.create({
-  baseURL: API_URL,
+  // baseURL: API_URL,
+  baseURL
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: API_URL,
+  baseURL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
