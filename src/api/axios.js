6100a24cf972d7a069d4179fcd764501829e1ce4
Aamin88 <forkahamin@yahoo.co.uk>
2024-09-30 02:28:49 +0100

    change api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,16 +4,16 @@ const baseURL = "https://vote-server-ahg0.onrender.com";
 // https://vote-server-ahg0.onrender.com
 // const baseURL = "https://vote-server-ahg0.onrender.com";
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 
 export default axios.create({
-  baseURL: API_URL,
-  // baseURL,
+  // baseURL: API_URL,
+  baseURL,
 });
 
 export const axiosPrivate = axios.create({
-  // baseURL,
-  baseURL: API_URL,
+  baseURL,
+  // baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
