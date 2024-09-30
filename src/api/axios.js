db0742c5b5768408e6af2ad8a4e7dd518a6823d0
Aamin88 <forkahamin@yahoo.co.uk>
2024-09-30 02:23:51 +0100

    add to axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+<<<<<<< HEAD
 const baseURL = "https://vote-server-ahg0.onrender.com";
 // https://vote-server-ahg0.onrender.com
+=======
+// const baseURL = "https://vote-server-ahg0.onrender.com";
+>>>>>>> parent of 3517bb8 (add:fixed cors)
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default axios.create({
-  // baseURL: API_URL,
-  baseURL,
+  baseURL: API_URL,
+  // baseURL,
 });
 
 export const axiosPrivate = axios.create({
